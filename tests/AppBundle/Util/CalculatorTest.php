4e30b9ada0c323a7e7238a4152fb0a3a03057cd0
Gregor <g.wawrzak@gmail.com>
2017-07-14 16:57:17 +0200

    Update CalculatorTest.php
    Tests have to start with test_ or you need to add @test annotation to the method.

--- tests/AppBundle/Util/CalculatorTest.php
+++ tests/AppBundle/Util/CalculatorTest.php
@@ -6,7 +6,21 @@ use AppBundle\Util\Calculator;
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function add($a, $b)
+    public function test_add()
+    {
+        $calc = new Calculator();
+        $result = $calc->add(30, 12);
+        $this->assertEquals(42, $result);
+    }
+
+    // albo
+
+    /**
+     * @test
+     * @param $a
+     * @param $b
+     */
+    public function add()
     {
         $calc = new Calculator();
         $result = $calc->add(30, 12);
